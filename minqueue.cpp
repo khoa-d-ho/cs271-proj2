48f2a2ad468f2344fc1c202f04d66df2460a2ae4
Hoangngo023 <122911494+Hoangngo023@users.noreply.github.com>
2024-02-20 05:28:57 +0700

    here you go

--- minqueue.cpp
+++ minqueue.cpp
@@ -10,13 +10,15 @@ template <class T>
 }
 
 template <class T>
-MinQueue<T>::MinQueue(T* array, int arraySize) {
+MinQueue<T>::MinQueue(T* array, int arraySize) 
+{
     heap = new T[capacity];
-    for (int i = 0; i < arraySize; ++i) {
+    for (int i = 0; i < arraySize; i++) 
+    {
         heap[i] = array[i];
     }
     for (int i = (heapSize / 2) - 1; i >= 0; --i) {
-        minHeapify(i);
+        heapify(i);
     }
 }
 
@@ -35,22 +37,24 @@ void MinQueue<T>::insert(const T& value) {
     heap[i] = value;
     
     while (i != 0 && heap[parent(i)] > heap[i]) {
-        std::swap(heap[i], heap[parent(i)]);
+        swap(heap[i], heap[parent(i)]);
         i = parent(i);
     }
 }
 
 template <class T>
 T MinQueue<T>::min() const {
-    if (isEmpty())
-        throw std::runtime_error("MinQueue is empty");
+    if (isEmpty()) {
+        cout << "MinQueue is empty." << endl;
+        return T();
     return heap[0];
 }
 
 template <class T>
 T MinQueue<T>::extractMin() {
-    if (isEmpty())
-        throw std::runtime_error("MinQueue is empty");
+    if (isEmpty()) {
+        cout << "MinQueue is empty." << endl;
+        return T();
     if (heapSize == 1) {
         heapSize--;
         return heap[0];
@@ -68,13 +72,62 @@ template <class T>
 void MinQueue<T>::decreaseKey(int i, T newValue) {
     heap[i] = newValue;
     while (i != 0 && heap[parent(i)] > heap[i]) {
-        std::swap(heap[i], heap[parent(i)]);
+        swap(heap[i], heap[parent(i)]);
         i = parent(i);
     }
 }
 
+template <class T>
+void MinQueue<T>::heapify(int i) 
+{
+    int l = left(i);
+    int r = right(i);
 
+    if (l <= heapSize && heap[l] < heap[smallest]) 
+    {
+        smallest = l;
+    }
+    else smallest = i;
 
+    if (r <= heapSize && heap[r] < heap[smallest]) 
+    {
+        smallest = r;
+    }
+    if (smallest != i) 
+    {
+        swap(heap[i], heap[smallest]);
+        heapify(smallest);
+    }
+}
+
+template <class T>
+void MinQueue<T>::build_min_heap(void) 
+{
+    headSize = T[capacity]
+    for (int i = (heapSize / 2) - 1; i >= 0; i--) 
+    {
+        heapify(i);
+    }
+}
+
+template <class T>
+void MinQueue<T>::sort(T* outArray) 
+{
+    build_min_heap();
+    
+    int originalSize = heapSize;
+    
+    for (int i = 0; i < originalSize; i++) 
+    {
+        outArray[i] = extractMin(); 
+    }
+
+    heapSize = originalSize;
+    for (int i = 0; i < heapSize; i++) 
+    {
+        insert(outArray[i]);
+    }
+}
 
 
 
